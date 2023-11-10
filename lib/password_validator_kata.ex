6fa4d81d1ffae9b6119e26d469d8b093a45414ee
Emanuele DelBono <emanuele@codiceplastico.com>
2023-11-10 15:44:02 +0100

    iteration 2

--- lib/password_validator_kata.ex
+++ lib/password_validator_kata.ex
@@ -8,4 +8,37 @@ defmodule PasswordValidatorKata do
     && String.contains?(pwd, "_")
   end
 
+  def more_than_8_chars() do
+    fn pwd -> String.length(pwd) > 8 end
+  end
+
+  def contains_a_capital_letter() do
+    fn pwd -> Regex.match?(~r/[A-Z]+/, pwd) end
+  end
+
+  def contains_a_lowercase_letter() do
+    fn pwd -> Regex.match?(~r/[a-z]+/, pwd) end
+  end
+
+  def contains_a_number() do
+    fn pwd -> Regex.match?(~r/[0-9]+/, pwd) end
+  end
+
+  def contains_an_underscore() do
+    fn pwd -> String.contains?(pwd, "_") end
+  end
+
+  def build([]), do: fn _ -> true end
+  def build(rules) do
+    fn pwd ->
+      apply_rules(pwd, rules)
+    end
+  end
+
+  def apply_rules(_, []), do: true
+  def apply_rules(pwd, [rule | t]) do
+    apply_rules(pwd, t) && rule.(pwd)
+  end
+
+
 end
--- test/password_validator_kata_test.exs
+++ test/password_validator_kata_test.exs
@@ -28,4 +28,67 @@ defmodule PasswordValidatorKataTest do
     refute PasswordValidatorKata.validate("has-more-than-8-chars-and-One-capital-lowercase-chars")
   end
 
+  test "Rule: Have more than 8 characters" do
+    rule = PasswordValidatorKata.more_than_8_chars()
+    assert rule.("pwd_with_more_than_eight_chars")
+    refute rule.("pwd")
+  end
+
+  test "Rule: Contains a capital letter" do
+    rule = PasswordValidatorKata.contains_a_capital_letter()
+    assert rule.("pWd")
+    refute rule.("pwd")
+  end
+
+  test "Rule: Contains a lowercase letter" do
+    rule = PasswordValidatorKata.contains_a_lowercase_letter()
+    assert rule.("pW")
+    refute rule.("PW")
+  end
+
+  test "Rule: Contains a number" do
+    rule = PasswordValidatorKata.contains_a_number()
+    assert rule.("8")
+    refute rule.("P")
+  end
+
+  test "Rule: Contains an underscore" do
+    rule = PasswordValidatorKata.contains_an_underscore()
+    assert rule.("_")
+    refute rule.("P")
+  end
+
+  test "build a password validator with no rules" do
+    validator = PasswordValidatorKata.build([])
+    assert validator.("pwd")
+  end
+
+  test "build a password validator with one rule" do
+    rule = PasswordValidatorKata.more_than_8_chars()
+    validator = PasswordValidatorKata.build([rule])
+    assert validator.("pwd_with_more_than_eight_chars")
+    refute validator.("short")
+  end
+
+  test "build a password validator with two rules" do
+    rule1 = PasswordValidatorKata.more_than_8_chars()
+    rule2 = PasswordValidatorKata.contains_a_capital_letter()
+    validator = PasswordValidatorKata.build([rule1, rule2])
+
+    assert validator.("Pwd_with_more_than_eight_chars")
+    refute validator.("short")
+    refute validator.("pwd_with_more_than_eight_chars")
+  end
+
+  test "build a password validator with three rules" do
+    rule1 = PasswordValidatorKata.more_than_8_chars()
+    rule2 = PasswordValidatorKata.contains_a_capital_letter()
+    rule3 = PasswordValidatorKata.contains_an_underscore()
+    validator = PasswordValidatorKata.build([rule1, rule2, rule3])
+
+    assert validator.("Pwd_with_more_than_eight_chars")
+    refute validator.("short_")
+    refute validator.("pwd_with_more_than_eight_chars")
+    refute validator.("pwd-with-more-than-eight-chars")
+  end
 end
